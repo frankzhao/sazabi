fe99b240ea3829dea7a4b08b0027dbf632ed99bf
Frank Zhao <frank@frankzhao.net>
2018-04-09 00:16:53 +1200

    Add timestamp to logging

--- sazabi/types/__init__.py
+++ sazabi/types/__init__.py
@@ -5,7 +5,13 @@ from abc import abstractmethod
 class LoggedObject(object):
     @property
     def logger(self):
-        return logging.getLogger(__name__)
+        formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+        logger = logging.getLogger(__name__)
+        handler = logging.FileHandler("sazabi.log")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
 
 
 class SazabiBotPlugin(LoggedObject):
