2b8a3128d77ec53ee2f699281c96b4f558e5e66e
Frank Zhao <frank@frankzhao.net>
2018-08-19 19:32:54 +1200

    Make logging configurable

--- sazabi/types/__init__.py
+++ sazabi/types/__init__.py
@@ -7,13 +7,7 @@ from abc import abstractmethod
 class LoggedObject(object):
   @property
   def logger(self):
-    formatter = logging.Formatter(
-        fmt='%(asctime)s %(levelname)-8s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
-    handler = logging.FileHandler(os.getenv("SAZABI_LOG", "/var/log/sazabi.log"))
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
     return logger
 
 
