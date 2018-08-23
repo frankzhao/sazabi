5078231c07615169935316690885634ea2761c51
Oliver Elias <oliver.elias@outlook.com>
2018-08-23 18:15:49 +1000

    Fix hardcoded tweet id (woops)

--- sazabi/plugins/twitter.py
+++ sazabi/plugins/twitter.py
@@ -7,11 +7,18 @@ class Twitter(SazabiBotPlugin):
     async def parse(self, client, message, *args, **kwargs):
         twitter_client = kwargs.get('twitter')
         if message.content.startswith('https://twitter.com/'):
-            status = twitter_client.show_status(id='1032512403890614273')
-            if len(status['extended_entities']['media']) > 1:
-                extra_images = list(map(
-                    lambda media: media['media_url_https'], 
-                    status['extended_entities']['media'][1:]
-                ))
-            if status is not None:
-                await client.send_message(message.channel, {files: extra_images})
+            status = twitter_client.show_status(id=message.content.split('/')[-1])
+            try:
+                if len(status['extended_entities']['media']) > 1:
+                    extra_images = list(map(
+                        lambda media: media['media_url_https'], 
+                        status['extended_entities']['media'][1:]
+                    ))
+                if status is not None:
+                    await client.send_message(message.channel, '\n'.join(extra_images))
+            except KeyError:
+                pass
+            except Exception as e:
+                self.logger.exception('Something went wrong grabbing tweet')
+                raise e
+
