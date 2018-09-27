0e4f57120ab5d6df1ec57b307801f3c5646ddbbb
Frank Zhao <frank@frankzhao.net>
2018-09-27 18:37:14 +1200

    Fix cat plugin
    Add more error handing for twitch

--- sazabi/plugins/cat.py
+++ sazabi/plugins/cat.py
@@ -8,10 +8,12 @@ class Cat(SazabiBotPlugin):
     async def parse(self, client, message, *args, **kwargs):
         if message.content == "~cat":
             self.logger.debug('Processing cat command')
-            cat_urls = ['http://thecatapi.com/api/images/get', "http://random.cat/meow"]
+            cat_urls = ['http://thecatapi.com/api/images/get', "http://aws.random.cat/meow"]
             cat_url = choice(cat_urls)
             if 'thecatapi' in cat_url:
                 cat = requests.get(cat_url).url
             else:
                 cat = requests.get(cat_url).json().get('file')
-            hold = await client.send_message(message.channel, cat)
+
+            if cat:
+                await client.send_message(message.channel, cat)
--- sazabi/plugins/twitch.py
+++ sazabi/plugins/twitch.py
@@ -7,6 +7,7 @@ from discord.errors import HTTPException
 from sazabi.model import Channel
 from sazabi.types import SazabiBotPlugin
 from sazabi.util import create_session
+from requests.exceptions import ConnectionError
 
 
 class Twitch(SazabiBotPlugin):
@@ -18,41 +19,45 @@ class Twitch(SazabiBotPlugin):
 
     for channel in session.query(Channel).all():
       user_login = channel.channel_name
-      response = requests.get('https://api.twitch.tv/helix/streams?user_login='
-                              + user_login, headers=headers)
-      if response.status_code == 200:
-        result = json.loads(response.text)
-        streams = result.get('data')
+      try:
+        response = requests.get('https://api.twitch.tv/helix/streams?user_login='
+                                + user_login, headers=headers)
+        if response.status_code == 200:
+          result = json.loads(response.text)
+          streams = result.get('data')
 
-        # update status
-        channel = session.query(Channel).filter(
-            Channel.channel_name == user_login).first()
+          # update status
+          channel = session.query(Channel).filter(
+              Channel.channel_name == user_login).first()
 
-        notify = False  # send a message update when going online
-        if len(streams) > 0:
-          if not channel.live:
-            # became live
-            notify = True
-          self.logger.debug('Stream {} is live'.format(user_login))
-          channel.live = True
+          notify = False  # send a message update when going online
+          if len(streams) > 0:
+            if not channel.live:
+              # became live
+              notify = True
+            self.logger.debug('Stream {} is live'.format(user_login))
+            channel.live = True
+          else:
+            channel.live = False
+            self.logger.debug('Stream {} is offline'.format(user_login))
+          channel.last_updated = datetime.now()
+
+          if notify:
+            channel.last_change = datetime.now()
+            self.logger.info(
+              'Send update, {} went live'.format(channel.channel_name))
+            await self.send_update(client, channel.channel_name)
+          else:
+            self.logger.info('No stream changes')
+          session.commit()
+
         else:
-          channel.live = False
-          self.logger.debug('Stream {} is offline'.format(user_login))
-        channel.last_updated = datetime.now()
+          self.logger.error(
+              "Could not connect to twitch: {}, {}".format(response.status_code,
+                                                           response.text))
+      except ConnectionError:
+        self.logger.error("Could not connect to twitch")
 
-        if notify:
-          channel.last_change = datetime.now()
-          self.logger.info(
-            'Send update, {} went live'.format(channel.channel_name))
-          await self.send_update(client, channel.channel_name)
-        else:
-          self.logger.info('No stream changes')
-        session.commit()
-
-      else:
-        self.logger.error(
-            "Could not connect to twitch: {}, {}".format(response.status_code,
-                                                         response.text))
 
     # close the session
     session.close()
